a767142122bd7a54923378abd55cb24f04757f6e
Daniel Warwick <danielwarwick14@gmail.com>
2015-07-22 11:20:01 +0100

    Improved the animation

--- src/js/example.js
+++ src/js/example.js
@@ -4,6 +4,14 @@ var timeline1 = new TimelineMax();
 var timeline2 = new TimelineMax();
 var timeline3 = new TimelineMax();
 
+var enabled = true;
+
+var width = window.innerWidth;
+
+if(width <= 640) enabled = false;
+
+if(enabled) {
+
 var scene1 = new ScrollMagic.Scene({
 
   duration:1100,
@@ -19,7 +27,7 @@ var tween1 = TweenMax.to('#ball', 1.5, {
   ease: Linear.easeNone
 });
 var tween1b = TweenMax.to('#ball', 1.5, {
-  left: 750,
+  left: 0.7*width,
   ease: Quad.easeIn
 });
 var tween2 = new TweenMax.to('#ball', 1.5, {
@@ -29,7 +37,7 @@ var tween2 = new TweenMax.to('#ball', 1.5, {
 });
 var tween2b = new TweenMax.to('#ball', 1.5, {
 
-  left: 50,
+  left: 0.1*width,
   ease: Quad.easeIn
 
 });
@@ -42,7 +50,7 @@ var tween3 = new TweenMax.to('#ball', 1, {
 });
 var tween3b = new TweenMax.to('#ball', 1, {
 
-  left:750,
+  left:0.7*width,
   ease:Quad.easeIn
 
 });
@@ -68,6 +76,39 @@ var tween3Hold = new TweenMax.to('#ball', 1, {
 
   top: 3220,
   ease: Linear.easeNone
+
+});
+var textTween1 = new TweenMax.to('#textbox1', 1.5, {
+
+  opacity:1,
+  delay:1
+
+});
+var textTween1b = new TweenMax.to('#textbox1', 2, {
+
+  opacity: 0
+
+});
+var textTween2 = new TweenMax.to('#textbox2', 2, {
+
+  top: 1700,
+  ease: SlowMo.ease.config(0.7, 0.7, false)
+
+});
+var textTween2b = new TweenMax.to('#textbox2', 2, {
+
+  opacity:1
+
+});
+var textTween3 = new TweenMax.to('#textbox3', 2, {
+
+  top: 2900,
+  ease: SlowMo.ease.config(0.7, 0.7, false)
+
+});
+var textTween3b = new TweenMax.to('#textbox3', 2, {
+
+  opacity:1
 
 });
 
@@ -128,10 +169,52 @@ var bgScene3 = new ScrollMagic.Scene({
 
 }).setTween(tweenScene3).addIndicators();
 
+var textScene1 = new ScrollMagic.Scene({
+
+  offset: 0,
+  duration: 100
+
+}).setTween(textTween1b).addIndicators();
+
+var textScene2 = new ScrollMagic.Scene({
+
+  offset: 1200,
+  duration: 600
+
+}).setTween(textTween2).addIndicators();
+
+var textScene2b = new ScrollMagic.Scene({
+
+  offset:1200,
+  duration:150
+
+}).setTween(textTween2b).addIndicators();
+
+var textScene3 = new ScrollMagic.Scene({
+
+  offset:2400,
+  duration:600
+
+}).setTween(textTween3).addIndicators();
+
+var textScene3b = new ScrollMagic.Scene({
+
+  offset:2400,
+  duration:150
+
+}).setTween(textTween3b).addIndicators();
+
 controller.addScene(scene1);
 controller.addScene(scene1Hold);
+controller.addScene(textScene1);
 controller.addScene(bgScene2);
 controller.addScene(scene2);
+controller.addScene(textScene2);
 controller.addScene(bgScene3);
 controller.addScene(scene3Hold);
 controller.addScene(scene3);
+controller.addScene(textScene2b);
+controller.addScene(textScene3);
+controller.addScene(textScene3b);
+
+}
